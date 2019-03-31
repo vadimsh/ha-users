c01e396ed218ce928dfa6fcfae281449ce11f8f3
Vadim.Shechter <vadim.shechter@sizmek.com>
2019-03-31 05:51:25 +0300

    add sudo to docker run

--- scripts/start-postgres.sh
+++ scripts/start-postgres.sh
@@ -6,7 +6,7 @@ POSTGRES_DB=$(cat .env | grep DB_DATABASE | cut -d = -f 2)
 
 echo $POSTGRES_DB:$POSTGRES_USER:$POSTGRES_PASSWORD
 
-docker run --name ha-pg -p 5432:5432 \
+sudo ocker run --name ha-pg -p 5432:5432 \
     -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
     -e POSTGRES_USER=${POSTGRES_USER} \
     -e POSTGRES_DB=${POSTGRES_DB} \
